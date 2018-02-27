075f9322027b1d2ac494b4e2b879895e05a72c2a
Hkiller <hkiller520@gmail.com>
2018-02-27 03:32:27 -0800

    update

---
+++ 2_12.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+std::string global_str;
+int global_int;
+
+int main(){
+int _;
+//int catch-22;
+//int 1_or_2 = 1;
+double Double = 3.14;
+    return 0;
+}
